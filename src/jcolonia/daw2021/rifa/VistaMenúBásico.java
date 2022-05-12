7969804fc186e56257d33437987b008959dfef10
ferny <ferny@LAPTOP-7USQJC5P>
2022-05-12 20:54:28 +0200

    Documentación

--- src/jcolonia/daw2021/rifa/VistaMenúBásico.java
+++ src/jcolonia/daw2021/rifa/VistaMenúBásico.java
@@ -4,15 +4,30 @@ import static java.lang.System.out;
 
 import java.util.Scanner;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class VistaMenúBásico.
+ */
 public class VistaMenúBásico extends Vista{
 
+	/** The opciones menú principal. */
 	private String[] opcionesMenúPrincipal;
 
+	/**
+	 * Instantiates a new vista menú básico.
+	 *
+	 * @param texto the texto
+	 * @param entrada the entrada
+	 * @param opcionesMenúPrincipal the opciones menú principal
+	 */
 	public VistaMenúBásico(String texto, Scanner entrada, String[] opcionesMenúPrincipal) {
 		super(texto, entrada);
 		this.opcionesMenúPrincipal=opcionesMenúPrincipal;
 	}
 
+	/**
+	 * Mostrar opciones.
+	 */
 	public void mostrarOpciones() {
 		out.println("----------------------------------------");
 		for (int i = 0; i < opcionesMenúPrincipal.length; i++) {
@@ -22,6 +37,11 @@ public class VistaMenúBásico extends Vista{
 		out.println("----------------------------------------\n");
 	}
 
+	/**
+	 * Pedir opción.
+	 *
+	 * @return the int
+	 */
 	public int pedirOpción() {
 		String lineaTexto;
 		int número = 0;
